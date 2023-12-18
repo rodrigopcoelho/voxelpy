2495a9b7d0550961e60c18bc199f097c0516f5b3
Rodrigo Coelho <rodrigocoelho@Rodrigos-MacBook-Pro.local>
2023-12-18 13:58:52 +0000

    trying

---
+++ shader_program.py
@@ -0,0 +1,32 @@
+from settings import *
+
+
+class ShaderProgram:
+    def __init__(self, app):
+        self.app = app
+        self.ctx = app.ctx
+        self.player = app.player
+        # -------- shaders -------- #
+        self.chunk = self.get_program(shader_name='chunk')
+        self.voxel_marker = self.get_program(shader_name='voxel_marker')
+        self.water = self.get_program('water')
+        self.clouds = self.get_program('clouds')
+        # ------------------------- #
+        self.set_uniforms_on_init()
+
+
+    def update(self):
+        self.chunk['m_view'].write(self.player.m_view)
+        self.voxel_marker['m_view'].write(self.player.m_view)
+        self.water['m_view'].write(self.player.m_view)
+        self.clouds['m_view'].write(self.player.m_view)
+
+    def get_program(self, shader_name):
+        with open(f'shaders/{shader_name}.vert') as file:
+            vertex_shader = file.read()
+
+        with open(f'shaders/{shader_name}.frag') as file:
+            fragment_shader = file.read()
+
+        program = self.ctx.program(vertex_shader=vertex_shader, fragment_shader=fragment_shader)
+        return program
